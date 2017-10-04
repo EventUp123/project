5ef8f015f8df43453270580b3909dcb6a2d4013c
KevinLuc1 <kevinluc1993@gmail.com>
2017-10-03 20:25:04 -0700

    nothing

---
+++ assets/js/typed.js
@@ -0,0 +1,8 @@
+
+$("#sportTag").hover(function(){
+  console.log("hovering");
+  $(".writeMe1").typed({
+    strings: ["Sports"],
+    typeSpeed: 150,
+  })
+})
---
+++ assets/js/typedLogic.js
@@ -0,0 +1,8 @@
+console.log("typedlogic is connected")
+$("#sportTag").hover(function(){
+  
+  $(".writeMe1").typed({
+    strings: ["Sports"],
+    typeSpeed: 150,
+  })
+})
